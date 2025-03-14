0bd102645b01a43bc0df59a4ff63f0986862346b
Harouna <harounacoulibaly74@yahoo.com>
2025-03-14 22:17:54 +0100

    modif

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 mkdir -p ~/.config/nvim && cat init.vim > ~/.config/nvim/init.vim
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && cat .tmux.conf > ~/.tmux.conf
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm & cat tmux.conf > ~/.tmux.conf
